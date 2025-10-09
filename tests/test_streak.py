4822b6f5f29ae136b15768fe1f280fec2dc416ff
Siddharth-magesh <siddharthmagesh007@gmail.com>
2025-10-09 12:15:47 +0000

    3

--- tests/test_streak.py
+++ tests/test_streak.py
@@ -1,21 +1,22 @@
-import pytest
+# tests/test_streak.py
 
+import pytest
 from streak import longest_positive_streak
 
-
-def test_empty():
+def test_empty_list():
     assert longest_positive_streak([]) == 0
 
-
-def test_all_positive():
-    assert longest_positive_streak([1, 1, 1]) == 3
-
+def test_single_streak():
+    assert longest_positive_streak([1, 2, 3]) == 3
 
 def test_multiple_streaks():
-    data = [2, 3, -1, 5, 6, 7, 0, 4]
-    # streaks: [2,3] -> 2 ; [5,6,7] -> 3 ; [4] -> 1
-    assert longest_positive_streak(data) == 3
-
+    assert longest_positive_streak([2, 3, -1, 5, 6, 7, 0, 4]) == 3
 
 def test_zeros_and_negatives():
-    assert longest_positive_streak([0, -1, 2, 3, 0, -5, 1, 2]) == 2
+    assert longest_positive_streak([0, -1, 2, 3, -5, 4, 1]) == 2
+
+def test_all_non_positive():
+    assert longest_positive_streak([0, -1, -2]) == 0
+
+def test_mixed_longest_at_end():
+    assert longest_positive_streak([-1, 0, 1, 2, 3]) == 3
